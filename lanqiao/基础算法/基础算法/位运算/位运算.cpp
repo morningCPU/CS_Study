7e134d9ae7e3a14d84afa28261f704313e424a7d
“monday-maker” <1512753709@qq.com>
2025-01-27 18:54:41 +0800

    完成位运算学习

--- lanqiao/基础算法/基础算法/位运算/位运算.cpp
+++ lanqiao/基础算法/基础算法/位运算/位运算.cpp
@@ -1,10 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+const int N = 1e6 + 9;
+using ll = long long;
+int a[N];
+int f[N];
+int p = 1e9 + 7;
 int main()
 {
-    unsigned int n;
+    int n;
     cin >> n;
-    int ans = n & -n;
-    cout << ans;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        f[i] = f[i - 1] ^ a[i];
+    }
+    ll ans = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = i; j <= n; ++j)
+        {
+            ans = ans * (f[j] ^ f[i - 1]);
+        }
+    }
+    cout << ans % p;
     return 0;
 }
